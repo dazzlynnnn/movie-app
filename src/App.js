4f7707093c5bce281ef9e033166a2ce35770f1bd
Lynn <dazzlynnnn@gmail.com>
2021-04-27 23:36:37 +0900

    #2 컴포넌트 설정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import React from "react"; 
+import Movie from "./Movie";
 
-function App() {
-  return (
-    <div>Hello EFUB</div>
-  );
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 3000);
+  }
+  render() {
+    const {isLoading, movies} = this.state;
+    return(
+      <section className="container">
+        {isLoading
+        ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        )
+        : (
+          <div className="movies">
+            <Movie/>
+          </div>
+        )}
+      </section>
+    );
+  }
 }
 
 export default App;
+
